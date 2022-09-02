6a4b264039ed51962ee4592195ffbb1c505c714f
KMRRingQA <62283469+KMRRingQA@users.noreply.github.com>
2022-09-02 12:49:03 +0100

    customer CRUD frontend finished

--- src/main/client/src/components/CustomerInputs.jsx
+++ src/main/client/src/components/CustomerInputs.jsx
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Error from "./Error";
 import axios from "axios";
+import Popup from "../components/Popup";
+import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
 
 const CustomerInputs = ({ createNew, customerId }) => {
-  const [DoBError, setDoBError] = useState(<></>);
-  const [PostcodeError, setPostcodeError] = useState(<></>);
-  const [PhoneError, setPhoneError] = useState(<></>);
-  const [EmailError, setEmailError] = useState(<></>);
-  const [CustomerSuccess, setCustomerSuccess] = useState(<></>);
-  const [CustomerError, setCustomerError] = useState(<></>);
+
+  const [customerDeleted, setCustomerDeleted] = useState(false);
+  const [customerCreated, setCustomerCreated] = useState(false);
+  const [customerUpdated, setCustomerUpdated] = useState(false);
+
+  const [popupContent, setPopUpContent] = useState(<></>);
+
+  const [notNullFields, setNotNullFields] = useState(false);
+  const [titleError, setTitleError] = useState(true);
+  const [surnameError, setSurnameError] = useState(true);
+  const [firstNameError, setFirstNameError] = useState(true);
+  const [doBError, setDoBError] = useState(false);
+  
+  // const [genderError, setGenderError] = useState(true);
+  const [customerTypeError, setCustomerTypeError] = useState(true);
+  const [address1Error, setAddress1Error] = useState(true);
+  // const [address2Error, setAddress2Error] = useState(false);
+  const [cityTownError, setCityTownError] = useState(true);
+  const [postcodeError, setPostcodeError] = useState(true);
+  const [phoneError, setPhoneError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  // const [placeOfBirthError, setPlaceOfBirthError] = useState(true);
+  // const [motherMaidenNameError, setMotherMaidenNameError] = useState(true);
+  const [customerSuccess, setCustomerSuccess] = useState(false);
+  const [customerError, setCustomerError] = useState(false);
   const customerData = JSON.parse(sessionStorage.getItem(`customer-${customerId}`));
 
   useEffect(() => {
@@ -16,35 +37,81 @@ const CustomerInputs = ({ createNew, customerId }) => {
   }, []);
 
   useEffect(() => {
-    !createNew && popoulateInputValues();
-  }, []);
+    console.log(titleError === false && surnameError === false && firstNameError === false && doBError === false && 
+      customerTypeError === false && address1Error === false && cityTownError === false && postcodeError === false && phoneError === false &&
+      emailError === false);
+    if (createNew) {
+      if (titleError === false && surnameError === false && firstNameError === false && doBError === false && 
+        customerTypeError === false && address1Error === false && cityTownError === false && postcodeError === false && phoneError === false &&
+        emailError === false) {
+          const customerType = document.getElementById("cons-radio").checked ? "Consumer" : "Corporate";
+          const customerGender = !document.getElementById("m-radio").checked ? !document.getElementById("f-radio").checked ? "X" : "F" : "M";
+        let customer = {
+          "title": notNullFields.title,
+          "surname": notNullFields.surname,
+          "firstName": notNullFields.firstName,
+          "dateOfBirth": document.getElementById("dob-input").value,
+          "gender": customerGender,
+          "customerType": customerType,
+          "address1": notNullFields.address1,
+          "address2": document.getElementById("address-2-input").value,
+          "cityTown": notNullFields.cityTown,
+          "postcode": notNullFields.postcode,
+          "phoneNo": document.getElementById("phone-input").value,
+          "email": document.getElementById("email-input").value,
+          "motherMaidenName": document.getElementById("mmn-input").value,
+          "placeOfBirth": document.getElementById("pob-input").value
+        }
+        axios
+          .post("http://localhost:9002/customer/create", customer)
+          .then((res) => {
+            console.log(res);
+            setCustomerCreated(true);
+            setPopUpContent(<><h2>Customer succesfully created</h2></>)
+          })
+          .catch((err) => setCustomerError(<Error message="account creation failed, ensure inputs are correct or contact system administrator" />));
+        console.log("Create customer");
+      }
+    }
+    if (!createNew) {
+      if (titleError === false && surnameError === false && firstNameError === false && doBError === false && 
+        customerTypeError === false && address1Error === false && cityTownError === false && postcodeError === false && phoneError === false &&
+        emailError === false) {
+          const customerType = document.getElementById("cons-radio").checked ? "Consumer" : "Corporate";
+          const customerGender = !document.getElementById("m-radio").checked ? !document.getElementById("f-radio").checked ? "X" : "F" : "M";
+        let customerUpdate = {
+          "id": customerData.id,
+          "title": notNullFields.title,
+          "surname": notNullFields.surname,
+          "firstName": notNullFields.firstName,
+          "dateOfBirth": document.getElementById("dob-input").value,
+          "gender": customerGender,
+          "customerType": customerType,
+          "address1": notNullFields.address1,
+          "address2": document.getElementById("address-2-input").value,
+          "cityTown": notNullFields.cityTown,
+          "postcode": notNullFields.postcode,
+          "phoneNo": document.getElementById("phone-input").value,
+          "email": document.getElementById("email-input").value,
+        }
+        axios
+          .put("http://localhost:9002/customer/update", customerUpdate)
+          .then((res) => {
+            console.log(res);
+            setCustomerUpdated(true);
+            setPopUpContent(<><h2>Customer succesfully updated</h2></>)
+            setCustomerError("");
+            // window.location.reload();
+          })
+          .catch((err) => setCustomerError(<Error message="account update failed, ensure inputs are correct or contact system administrator" />));
+        console.log("updated customer");
+      }
+    }
+  }, [notNullFields]);
 
   const submitChanges = () => {
-    let customerUpdate = {
-      "id": customerData.id,
-      "title": document.getElementById("title-select").value,
-      "surname": document.getElementById("sname-input").value,
-      "firstName": document.getElementById("fname-input").value,
-      "dateOfBirth": document.getElementById("dob-input").value,
-      "gender": document.getElementById("m-radio").value,
-      "customerType": document.getElementById("cons-radio").value,
-      "address1": document.getElementById("address-1-input").value,
-      "address2": document.getElementById("address-2-input").value,
-      "cityTown":document.getElementById("city-input").value,
-      "postcode":document.getElementById("postcode-input").value,
-      "phoneNo":document.getElementById("phone-input").value,
-      "email":document.getElementById("email-input").value,
-  }
-      axios
-      .put("http://localhost:9002/customer/update", customerUpdate)
-      .then((res) => {
-        console.log(res);
-        setCustomerSuccess(<h2>account succesfully updated</h2>)
-        setCustomerError("");
-        // window.location.reload();
-      })
-      .catch((err) => setCustomerError(<Error message= "account update failed, ensure inputs are correct or contact system administrator"/>));
-      console.log("updated customer");
+    validateUserValues();
+
   };
 
   const deleteCustomer = () => {
@@ -52,43 +119,48 @@ const CustomerInputs = ({ createNew, customerId }) => {
       .delete(`http://localhost:9002/customer/delete/${customerData.id}`)
       .then((res) => {
         console.log(res);
-        setCustomerSuccess(<h2>customer succesfully deleted</h2>)
-        window.location.href = "/customer-search"
+        setCustomerDeleted(true);
+        setPopUpContent(<><h2>Customer succesfully deleted</h2></>)
       })
-      .catch((err) => setCustomerError(<Error message= "cannot delete customer; acounts still exist under customer name."/>));
+      .catch((err) => setCustomerError(<Error message="cannot delete customer; acounts still exist under customer name." />));
     console.log("Delete customer");
   };
 
-  
 
   const createCustomer = () => {
-    let customer = {
-      "title": document.getElementById("title-select").value,
-      "surname": document.getElementById("sname-input").value,
-      "firstName": document.getElementById("fname-input").value,
-      "dateOfBirth": document.getElementById("dob-input").value,
-      "gender": document.getElementById("m-radio").value,
-      "customerType": document.getElementById("cons-radio").value,
-      "address1": document.getElementById("address-1-input").value,
-      "address2": document.getElementById("address-2-input").value,
-      "cityTown":document.getElementById("city-input").value,
-      "postcode":document.getElementById("postcode-input").value,
-      "phoneNo":document.getElementById("phone-input").value,
-      "email":document.getElementById("email-input").value,
-      "motherMaidenName":document.getElementById("mmn-input").value,
-      "placeOfBirth":document.getElementById("pob-input").value
-  }
-  axios
-      .post("http://localhost:9002/customer/create", customer)
-      .then((res) => {
-        console.log(res);
-        setCustomerSuccess(<h2>account succesfully created</h2>)
-      })
-      .catch((err) => setCustomerError(<Error message= "account creation failed, ensure inputs are correct or contact system administrator"/>));
-    console.log("Create customer");
+    validateUserValues()
   };
 
-  const validateDoB = () => {
+  const validateUserValues = () => {
+
+    checkTitle();
+    checkSurname();
+    checkFirstName();
+    checkCustomerType();
+    checkAddress1();
+    checkCityTown();
+    checkPostcode();
+    checkDoB();
+
+    setNotNullFields({
+      title: document.getElementById("title-select").value,
+      surname: document.getElementById("sname-input").value,
+      firstName: document.getElementById("fname-input").value,
+      customerType: document.getElementById("cons-radio").value,
+      address1: document.getElementById("address-1-input").value,
+      cityTown: document.getElementById("city-input").value,
+      postcode: document.getElementById("postcode-input").value,
+    })
+  }
+
+  const checkTitle = () => setTitleError(document.getElementById("title-select").value === "default" ? <Error message="Title cannot be blank" /> : false)
+  const checkSurname = () => setSurnameError(!document.getElementById("sname-input").value ? <Error message="Surname cannot be blank" /> : false)
+  const checkFirstName = () => setFirstNameError(!document.getElementById("fname-input").value ? <Error message="First Name cannot be blank" /> : false)
+  const checkCustomerType = () => setCustomerTypeError(!document.getElementById("cons-radio").checked && !document.getElementById("corp-radio").checked ? <Error message="Customer Type cannot be blank" /> : false) 
+  const checkAddress1 = () => setAddress1Error(!document.getElementById("address-1-input").value ? <Error message="Address 1 cannot be blank" /> : false)
+  const checkCityTown = () => setCityTownError(!document.getElementById("city-input").value ? <Error message="City/Town cannot be blank" /> : false)
+
+  const checkDoB = () => {
     const getAge = (dateString) => {
       var ageInMilliseconds = new Date() - new Date(dateString);
       return Math.floor(ageInMilliseconds / 1000 / 60 / 60 / 24 / 365);
@@ -104,12 +176,12 @@ const CustomerInputs = ({ createNew, customerId }) => {
       ) : getAge(input) > 65 ? (
         <Error message="Customer is too old" />
       ) : (
-        <></>
+        false
       )
     );
   };
 
-  const validatePostcode = () => {
+  const checkPostcode = () => {
     let input = document.querySelector("#postcode-input").value;
     setPostcodeError(
       !new RegExp(
@@ -117,12 +189,12 @@ const CustomerInputs = ({ createNew, customerId }) => {
       ).test(input) ? (
         <Error message="Invalid postcode format" />
       ) : (
-        <></>
+        false
       )
     );
   };
 
-  const validateEmail = () => {
+  const checkEmail = () => {
     let input = document.querySelector("#email-input").value;
     setEmailError(
       !new RegExp(
@@ -130,12 +202,12 @@ const CustomerInputs = ({ createNew, customerId }) => {
       ).test(input) ? (
         <Error message="Invalid email" />
       ) : (
-        <></>
+        false
       )
     );
   };
 
-  const validatePhone = () => {
+  const checkPhone = () => {
     const input = document.querySelector("#phone-input").value;
     setPhoneError(
       !new RegExp(
@@ -143,12 +215,12 @@ const CustomerInputs = ({ createNew, customerId }) => {
       ).test(input) ? (
         <Error message="Invalid phone number" />
       ) : (
-        <></>
+        false
       )
     );
   };
 
-  const checkPhone = () => {
+  const checkPhoneFormat = () => {
     const input = document.querySelector("#phone-input").value;
     document.querySelector("#phone-input").value = isNaN(input.value) && input.replace(/\D/g, "");
   };
@@ -187,8 +259,19 @@ const CustomerInputs = ({ createNew, customerId }) => {
     emailInput.value = customerData.email ? customerData.email : "N/A";
   };
 
+  const closePopUp = () =>{
+    if(customerCreated){
+      setCustomerCreated(false);
+      window.location.href="/create-account"
+    }
+    setCustomerDeleted(false);
+    setCustomerUpdated(false);
+    window.location.href="/customer-search";
+  }
+
   return (
     <div className="main-container">
+      {(customerCreated || customerDeleted || customerUpdated) && <Popup handleClose={closePopUp} content={popupContent}/>}
       {!createNew && (
         <div className="input-container">
           <span>Customer ID:</span>
@@ -199,7 +282,7 @@ const CustomerInputs = ({ createNew, customerId }) => {
       <div className="input-container">
         <span>Title:</span>
         <br />
-        <select defaultValue={"default"} id="title-select">
+        <select defaultValue={"default"} id="title-select" onBlur={checkTitle}>
           <option value="default" disabled />
           <option value="mr">Mr</option>
           <option value="mrs">Mrs</option>
@@ -213,17 +296,17 @@ const CustomerInputs = ({ createNew, customerId }) => {
       <div className="input-container">
         <span>Surname:</span>
         <br />
-        <input type="text" id="sname-input" />
+        <input type="text" id="sname-input" onBlur={checkSurname} />
       </div>
       <div className="input-container">
         <span>Forename:</span>
         <br />
-        <input type="text" id="fname-input" />
+        <input type="text" id="fname-input" onBlur={checkFirstName} />
       </div>
       <div className="input-container">
         <span>DoB:</span>
         <br />
-        <input type="text" onBlur={validateDoB} id="dob-input" />
+        <input type="text" onBlur={checkDoB} id="dob-input" />
       </div>
       <div className="input-container">
         <span>Sex:</span>
@@ -241,16 +324,16 @@ const CustomerInputs = ({ createNew, customerId }) => {
         <span>Type:</span>
         <br />
         <div>
-          <input type="radio" name="c-type" value="cons" id="cons-radio" className="first-radio" />
+          <input type="radio" name="c-type" value="cons" id="cons-radio" className="first-radio" onBlur={checkCustomerType} />
           <label htmlFor="c-type">Consumer</label>
-          <input type="radio" name="c-type" value="corp" id="corp-radio" />
+          <input type="radio" name="c-type" value="corp" id="corp-radio" onBlur={checkCustomerType} />
           <label htmlFor="c-type">Corporate</label>
         </div>
       </div>
       <div className="input-container">
         <span>Address 1:</span>
         <br />
-        <input type="text" id="address-1-input" />
+        <input type="text" id="address-1-input" onBlur={checkAddress1} />
       </div>
       <div className="input-container">
         <span>Address 2:</span>
@@ -260,22 +343,22 @@ const CustomerInputs = ({ createNew, customerId }) => {
       <div className="input-container">
         <span>City:</span>
         <br />
-        <input type="text" id="city-input" />
+        <input type="text" id="city-input" onBlur={checkCityTown} />
       </div>
       <div className="input-container">
         <span>Postcode:</span>
         <br />
-        <input type="text" id="postcode-input" onBlur={validatePostcode} />
+        <input type="text" id="postcode-input" onBlur={checkPostcode} />
       </div>
       <div className="input-container">
         <span>Phone No:</span>
         <br />
-        <input type="tel" id="phone-input" onBlur={validatePhone} onChange={checkPhone} />
+        <input type="tel" id="phone-input" onBlur={checkPhone} onChange={checkPhoneFormat} />
       </div>
       <div className="input-container">
         <span>Email:</span>
         <br />
-        <input type="email" id="email-input" onBlur={validateEmail} />
+        <input type="email" id="email-input" onBlur={checkEmail} />
       </div>
       {createNew && (
         <div>
@@ -308,14 +391,27 @@ const CustomerInputs = ({ createNew, customerId }) => {
           </>
         )}
       </div>
-      {DoBError}
-      {PostcodeError}
-      {PhoneError}
-      {EmailError}
-      {CustomerError}
-      {CustomerSuccess}
+      {doBError}
+      {postcodeError}
+      {phoneError}
+      {emailError}
+      {customerError}
+      {customerSuccess}
+      {titleError}
+      {surnameError}
+      {firstNameError}
+      {/* {genderError} */}
+      {customerTypeError}
+      {address1Error}
+      {/* {address2Error} */}
+      {cityTownError}
+      {/* {placeOfBirthError}
+      {motherMaidenNameError} */}
     </div>
+    
   );
+
 };
+
 
 export default CustomerInputs;
