e74695f1a52a1441e4f589dd470d83004c5148eb
tjayling <tjayling@yahoo.co.uk>
2022-09-14 10:33:28 +0100

    Broke phone number regex

--- src/main/client/src/components/CustomerInputs.jsx
+++ src/main/client/src/components/CustomerInputs.jsx
@@ -214,7 +214,7 @@ const CustomerInputs = ({ createNew, customerId }) => {
     const input = document.querySelector("#phone-input").value;
     setPhoneError(
       !new RegExp(
-        /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?(\d{4}|\d{3}))?$/
+        /^(((\+44\s?\d{4})\s?\d{3}\s?\d{3})|((\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2})\s?\d{4}\s?\d{4}))(\s?(\d{4}|\d{3}))?$/
       ).test(input) ? (
         <Error message="Invalid phone number" />
       ) : (
