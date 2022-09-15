303ebdb8bf66663f5ec8372248da119b63e4b0f0
KMRRingQA <62283469+KMRRingQA@users.noreply.github.com>
2022-09-15 11:26:06 +0100

    deleted bloat

--- src/test/java/com/qa/banking/BankingApplicationTests.java
+++ src/test/java/com/qa/banking/BankingApplicationTests.java
@@ -40,39 +40,4 @@ class BankingApplicationTests {
 		assertEquals(2,customerDtos.get(0).getAccounts().size());
 	}
 
-	@Test
-	public void test(){
-		String[] strings = {"cancel","peanut","harbor","throat","cattle","unfair","favour"};
-		String guess1 = "favour";
-		String guess2 = "cancel";
-		int result1 = 2;
-		int result2 = 3;
-
-		List<String> result = Arrays.stream(strings).filter(x-> {
-			if(MatchingCharacters(x,guess1)!=result1 || MatchingCharacters(x,guess2)!=result2 ){
-				return false;
-			} else return true;
-		}).collect(Collectors.toList());
-
-		System.out.println(result.get(0) + " " + result.size());
-	}
-
-		public int MatchingCharacters(String str1, String str2) {
-			char[] arr = str1.toCharArray();
-			char[] arr2 = str2.toCharArray();
-			HashMap<Character,Integer> hMap = new HashMap<>();
-			HashMap<Character,Integer> hMap2 = new HashMap<>();
-			for(int i = 0 ; i < arr.length ; i++) {
-				if(!hMap.containsKey(arr[i])) {
-					hMap.put(arr[i],1);
-				}
-			}
-			for(int i = 0 ;i <arr2.length ;i++) {
-				if(hMap.containsKey(arr2[i])) {
-					hMap2.put(arr2[i],1);
-				}
-			}
-			return hMap2.size();
-		}
-
 }
